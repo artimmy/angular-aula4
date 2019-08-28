3cbf85a86b80dceb5e90108ed94b4ec98c78688c
artimmy <arthurtimmy@hotmail.com>
2019-08-28 10:06:10 -0300

    2 files modified

--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicoService } from '../servico.service';
 
 @Component({
   selector: 'app-tabela',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabelaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private s:ServicoService) 
+  { 
+  }
 
   ngOnInit() {
   }
